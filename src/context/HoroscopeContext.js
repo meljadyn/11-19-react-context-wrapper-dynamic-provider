178b6d5d14abf31f00134ea646fab82890291a8f
Jadyn <33948021+meljadyn@users.noreply.github.com>
2022-08-17 17:44:19 -0400

    Initial commit

---
+++ src/context/HoroscopeContext.js
@@ -0,0 +1,16 @@
+import { useState, createContext } from "react";
+import horoscopesObj from "../data/horoscopes";
+
+export const HoroscopeContext = createContext();
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Pisces");
+  const sign = horoscopesObj[currentSign];
+
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
+
+export default HoroscopeProvider;
